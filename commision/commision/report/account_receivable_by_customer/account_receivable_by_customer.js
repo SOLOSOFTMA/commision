716886934911ae6b8ba883fe0e206840c8ba2131
Bobby <bobzz.zone@gmail.com>
2017-04-26 15:08:18 +0000

    new report

---
+++ commision/commision/report/account_receivable_by_customer/account_receivable_by_customer.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, bobzz.zone@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Account Receivable By Customer"] = {
+	"filters": [
+
+	]
+}
+
