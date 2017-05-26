0fabd1af8636ebd6b6f9da828ed3769ee50cbf2f
Bobby <bobzz.zone@gmail.com>
2017-05-26 15:03:31 +0000

    by sales doc

---
+++ commision/commision/doctype/sales_commision/test_sales_commision.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, bobzz and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Sales Commision')
+
+class TestSalesCommision(unittest.TestCase):
+	pass
