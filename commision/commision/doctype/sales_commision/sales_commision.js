f77fd7f86bb4c797987028166d5c7d6dc3fd0154
bobzz-zone <bobzz.zone@gmail.com>
2017-05-26 23:06:03 +0700

    a

--- commision/commision/doctype/sales_commision/sales_commision.js
+++ commision/commision/doctype/sales_commision/sales_commision.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on('Sales Commision', {
 	refresh: function(frm) {
-
+		cur_frm.add_fetch("sales","supervisor","supervisor");
+		cur_frm.add_fetch("period","to_date","to_date");
 	}
 });
